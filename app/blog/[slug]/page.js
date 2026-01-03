4cb8295343f412e732af78c17d5c363e82dd0218
butacoro <butacoro2011@gmail.com>
2026-01-03 17:27:32 +0900

    前後の記事へ移動するボタンの追加

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,29 +1,21 @@
 import Image from "next/image"
-import fs from "fs"
-import path from "path"
-import matter from "gray-matter"
 import ReactMarkdown from "react-markdown"
+import PrevNext from "@/app/components/prevNext"
+import { getAllBlogs,getSingleBlog } from "@/app/utils/mdQueries"
 
-async function getSingleBlog(params) {
-	console.log("***")
-	console.log(params)
-	const { slug } = params
-	console.log(slug)
-	const data = await import(`../../../data/${slug}.md`)
-	const singleDocument = matter(data.default)
-
-	return {
-		singleDocument: singleDocument
-	}
-}
 
 //const SingleBlog = (props) => {
 const SingleBlog = async ({ params, searchParams }) => {
 	const p = await params
 	const sp = await searchParams
-	console.log({ params: p, searchParams: sp })
+	//console.log({ params: p, searchParams: sp })
 
 	const {singleDocument} = await getSingleBlog(p)
+
+	const {blogs} = await getAllBlogs()
+	const prev = blogs.filter(blog => blog.frontmatter.id === singleDocument.data.id - 1)
+	const next = blogs.filter(blog => blog.frontmatter.id === singleDocument.data.id + 1)
+
 
 	return (
         <>
@@ -36,6 +28,7 @@ const SingleBlog = async ({ params, searchParams }) => {
                     <p>{singleDocument.data.date}</p> 
                     <ReactMarkdown>{singleDocument.content}</ReactMarkdown>
                 </div> 
+				<PrevNext prev={prev} next={next} />
             </div>
         </>
 	)
@@ -44,24 +37,6 @@ const SingleBlog = async ({ params, searchParams }) => {
 export default SingleBlog
 
 export async function generateStaticParams() {
-	async function getAllBlogs() {
-		const files = fs.readdirSync(path.join("data"))
-		const blogs = files.map((fileName) => {
-			const slug = fileName.replace(".md", "")
-			const fileData = fs.readFileSync(
-				path.join("data", fileName),
-				"utf-8"
-			)
-			const { data } = matter(fileData)
-			return {
-				frommatter: data,
-				slug: slug
-			}
-		})
-		return {
-			blogs: blogs
-		}
-	}
 
 	const { blogs } = await getAllBlogs()
 	//const paths = blogs.map((blog) => `/${blog.slug}`)
---
+++ app/utils/mdQueries.js
@@ -0,0 +1,40 @@
+import fs from "fs"       
+import path from "path" 
+import matter from "gray-matter"
+
+export async function getAllBlogs() {  
+	const files = fs.readdirSync(path.join("data"))
+	const blogs = files.map((fileName) => {
+		const slug = fileName.replace(".md", "")
+		const fileData = fs.readFileSync(
+			path.join("data", fileName),
+			"utf-8"
+		)
+		const { data } = matter(fileData) 
+		return {
+			frontmatter: data,
+			slug: slug, 
+		}
+	})
+
+	const orderedBlogs = blogs.sort((a, b) => {
+		return b.frontmatter.id - a.frontmatter.id
+	})
+
+	return{
+		blogs: orderedBlogs
+	}
+}
+
+//const SingleBlog = (props) => {
+export async function getSingleBlog(params) {
+	//console.log(params)
+	const { slug } = params
+	//console.log(slug)
+	const data = await import(`../../data/${slug}.md`)
+	const singleDocument = matter(data.default)
+
+	return {
+		singleDocument: singleDocument
+	}
+}
